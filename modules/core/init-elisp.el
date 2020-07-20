fc26c9984126fa0697f50d7bf8546c27e45cb89c
dwuggh <dwuggh@gmail.com>
2020-07-20 18:39:28 +0800

    add emacs lisp config

---
+++ modules/core/init-elisp.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t; -*-
+
+
+(dwuggh/localleader-def
+ :major-modes 'emacs-lisp-mode
+ "hh" '(helpful-at-point :wk "help at point")
+ "'" '(ielm :wk "open ielm")
+ )
+
+
+
+
+
+
+
+
+(provide 'init-elisp)
