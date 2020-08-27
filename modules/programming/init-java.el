9a471c7f62e9c301bc3dac9bdacf24781cc2b005
dwuggh <dwuggh@gmail.com>
2020-08-27 18:22:07 +0800

    add java

---
+++ modules/programming/init-java.el
@@ -0,0 +1,13 @@
+
+(use-package lsp-java
+  :defer t
+  :init
+  ;; (add-hook 'java-mode-hook (lambda ()
+  ;; 			      (require 'lsp-java)
+  ;; 			      (lsp)))
+  (setq lsp-java-workspace-dir (concat my-cache-dir "lsp/workspace/"))
+  (add-hook 'java-mode-hook #'lsp)
+  )
+
+
+(provide 'init-java)
