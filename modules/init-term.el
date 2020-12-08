b385c9920edf92aaee1c5af89ae799dc29ebc5a3
dwuggh <dwuggh@gmail.com>
2020-12-09 00:22:10 +0800

    fix: vterm cursor bug

--- modules/init-term.el
+++ modules/init-term.el
@@ -3,5 +3,15 @@
 
 (setq explicit-bash-args (cdr explicit-bash-args))
 
+(defun my-vterm-set-cursor-after-update (&rest args)
+  (if (equal evil-state 'insert)
+	(setq-local cursor-type '(bar . 2))
+	(setq-local cursor-type t)
+	)
+  )
 
+
+(advice-add 'vterm--update :after
+			'my-vterm-set-cursor-after-update
+			)
 (provide 'init-term)
