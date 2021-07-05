7b5da0b376fd2d312d175a0b3cfe2bd70d84b4e4
dwuggh <dwuggh@gmail.com>
2021-07-05 11:18:57 +0800

    fix tree-sitter packages

--- modules/programming/init-programming.el
+++ modules/programming/init-programming.el
@@ -1,48 +1,17 @@
 
-(use-package leetcode
-  :defer t
-  :config
-  (define-key leetcode--problems-mode-map (kbd "TAB") 'leetcode-show-current-problem)
-  (define-key leetcode--problems-mode-map (kbd "<return>") 'leetcode-show-current-problem)
-  (setq leetcode-directory "~/Documents/leetcode"
-    leetcode-prefer-language "cpp")
-  )
-
-
-;; (straight-register-package
-;;  '(tsc :host github
-;;        :repo "ubolonton/emacs-tree-sitter"
-;;        :no-build t
-;;        :files ("core/*.el"))
-;;  )
-
-;; (straight-use-package tsc)
-;; (straight--add-package-to-load-path
-;;  '(tsc :host github
-;;        :repo "ubolonton/emacs-tree-sitter"
-;;        :files ("core/*.el"))
-;;  )
-
-;; (with-temp-file tsc-dyn-get--version-file
-;;       (let ((coding-system-for-write 'utf-8))
-;;         (insert "0.12.0")))
-
 (setq load-path 
       (cons (expand-file-name
          (concat straight-base-dir "straight/build/tsc"))
         load-path))
 
+(defun my-url-copy-file (url newname &optional ok-if-already-exists &rest _ignored)
+  "See `url-copy-file'."
+  ;; TODO
+  )
 
-;; Base framework, syntax highlighting.
-(straight-use-package
- '(tree-sitter :host github
-               :repo "ubolonton/emacs-tree-sitter"
-               :files ("lisp/*.el")))
-;; Language bundle.
-(straight-use-package
- '(tree-sitter-langs :host github
-                     :repo "ubolonton/emacs-tree-sitter"
-                     :files ("langs/*.el" "langs/queries")))
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+
 
 (use-package tree-sitter-langs)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
