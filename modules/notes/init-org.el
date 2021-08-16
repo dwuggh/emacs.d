de251c2a2eb30314ee261e0642beea26dfe529ec
dwuggh <dwuggh@gmail.com>
2021-08-16 15:29:32 +0800

    organize org

--- modules/notes/init-org.el
+++ modules/notes/init-org.el
@@ -1,213 +1,25 @@
 (require 'lib-hack)
 
 ;; https://github.com/raxod502/straight.el/issues/211#issuecomment-355840069
-(straight-use-package '(org :local-repo nil))
-(straight-use-package '(org-plus-contrib :local-repo nil))
+;; (use-package git
+;;   :defer t)
 
-(setq org-src-window-setup 'split-window-below
-      org-hide-emphasis-markers t
-      )
+;; (defun org-git-version ()
+;;   (let ((git-repo
+;;      (f-join user-emacs-directory "straight/repos/org")))
+;;     (s-trim (git-run "describe"
+;;              "--match=release\*"
+;;              "--abbrev=6"
+;;              "HEAD"))))
 
-(setq org-hide-emphasis-markers nil)
-(defun org-toggle-emphasis-markers ()
-  "Toggle markup display."
-  (interactive)
-  (if org-hide-emphasis-markers
-      (setq org-hide-emphasis-markers nil)
-    (setq org-hide-emphasis-markers t))
-  (font-lock-flush)
-  )
-
-(use-package markdown-mode
-  :defer t
-  :init
-  (setq markdown-hide-markup t
-        markdown-hide-markup-in-view-modes t
-        markdown-command "marked"
-        )
-  ;; (add-to-list 'auto-mode-alist '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . gfm-mode))
-  :config
-  (dwuggh/localleader-def
-   :keymaps 'markdown-mode-map
-   "tt" 'markdown-toggle-markup-hiding
-   "tp" 'markdown-toggle-inline-images
-   "tm" 'markdown-toggle-math
-   )
-  )
-
-(use-package grip-mode
-  :defer t
-  :init
-  ;; (add-hook 'markdown-mode-hook #'grip-mode)
-  ;; (setq grip-preview-use-webkit t)
-  )
-
-;; (defun my-org-mode-hook ()
-;;   (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
-;; (add-hook 'org-mode-hook #'my-org-mode-hook)
-
-(use-package org-superstar
-  :defer t
-  :init (add-hook 'org-mode-hook 'org-superstar-mode))
-
-(setq org-id-locations-file (concat my-cache-dir ".org-id-locations"))
-
-(use-package evil-org)
-
-(use-package org-roam
-  ;; :defer t
-  :after org
-  ;; :hook
-  ;; (after-init . org-roam-mode)
-  :commands (org-roam-buffer
-             org-roam-setup
-             org-roam-capture
-             org-roam-node-find)
-  :init
-  (setq org-roam-directory (expand-file-name "~/org/roam/")
-        org-roam-db-location (concat my-cache-dir "org-roam.db")
-        org-id-link-to-org-use-id
-        )
-  :config
-  (org-roam-setup)
-  (advice-add 'org-id-get-create :after 'org-roam-db-sync)
-  )
-
-(defun org-latex-preview-hook ()
-  "hook for auto `org-latex-preview'"
-  (when org-mode-hook
-    (when (and (not (equal (buffer-name) "*Youdao Dictionary*")))
-      (org-latex-preview '(16))))
-  )
-
-(add-hook 'org-mode-hook #'org-latex-preview-hook)
-
-;; better table alignment
-;; TODO integration with org-latex-preview
-(use-package valign
-  :straight (valign :host github
-                    :repo "casouri/valign"
-                    )
-  :defer t
-  :init
-  (setq
-   valign-fancy-bar t
-   )
-  (add-hook 'org-mode-hook #'valign-mode))
-
-(defun lsp-flycheck-filter-org-mode (oldfun &rest args)
-  (unless (equal (car args) 'org-mode)
-    (apply oldfun args)))
-
-
-(advice-add 'lsp-flycheck-add-mode :around
-            #'lsp-flycheck-filter-org-mode)
-
-
-;;; latex editing
-;;; ------------------------------------------------------------------------------------
-
-;; auto preview latex fragments
-(setq-default org-preview-latex-image-directory
-              (concat user-emacs-directory ".cache/ltximg/"))
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (latex . t)   ;; <== add latex to the list
-   (python . t)
-   (shell . t)
-   ))
-
-
-;; set pdflatex to xelatex
-(setq-default org-latex-default-packages-alist
-              '(("AUTO" "inputenc" t
-                 ("xelatex"))
-                ("T1" "fontenc" t
-                 ("xelatex"))
-                ("" "graphicx" t nil)
-                ("" "longtable" nil nil)
-                ("" "wrapfig" nil nil)
-                ("" "rotating" nil nil)
-                ("normalem" "ulem" t nil)
-                ("" "amsmath" t nil)
-                ("" "textcomp" t nil)
-                ("" "amssymb" t nil)
-                ("" "capt-of" nil nil)
-                ("" "hyperref" nil nil)))
-
-;; customize packages
-(setq-default org-latex-packages-alist
-              '(
-                ("" "ctex" t nil)
-                ("" "braket" t nil)
-                ))
-
-
-
-
-
-(straight-use-package '(my-org-edit-latex :local-repo "~/Projects/emacs/org-edit-latex/"))
-(use-package my-org-edit-latex
-  ;; :straight '(org-edit-latex :local-repo "~/Projects/emacs/org-edit-latex/")
-  :defer t
-  :init
-  (setq org-edit-latex-default-frag-master
-    (concat user-emacs-directory "frag-master.tex"))
-  (add-hook 'org-mode-hook #'org-edit-latex-mode)
-  )
-
-;; configure latex preview
-;; from doom emacs, auto refresh background with theme
-(defun +org-refresh-latex-background-h (&rest args)
-      "Previews are rendered with the incorrect background.
-This forces it to read the background before rendering."
-      (plist-put+ org-format-latex-options
-                  :background
-                  (face-attribute (if-let (remap (cadr (assq 'default face-remapping-alist)))
-                                      (if (keywordp (car-safe remap))
-                                          (plist-get remap :background)
-                                        remap)
-                                      'default)
-                                  :background nil t)))
-
-(advice-add 'load-theme :after '+org-refresh-latex-background-h)
-
-(add-hook 'org-mode-hook
-      '+org-refresh-latex-background-h)
-
-(setq-default org-preview-latex-default-process 'dvipng)
-
-(plist-put+ org-format-latex-options
-        :scale 2.0
-        ;; specify the justification you want
-        ;; :justify 'center
-        )
-
-(add-hook 'org-mode-hook #'electric-pair-local-mode)
-
-(use-package org-ml
-  :straight (org-ml :type built-in
-                    :local-repo (concat user-emacs-directory "lisp/")
-                    :files ("org-ml.el"))
-  :after org
-  :init
-  (add-hook 'org-mode-hook 'org-ml-mode)
-  )
-
-(use-package company-org-latex
-  :straight (company-org-latex :type built-in
-                               :local-repo (concat user-emacs-directory "lisp/")
-                               :files ("company-org-latex.el"))
-  :after org
-  :init
-  (defun org-setup-company ()
-    (setq-local company-backends '(company-capf company-yasnippet company-dabbrev))
-    (add-hook 'completion-at-point-functions #'org-latex-capf nil t)
-    )
-  (add-hook 'org-mode-hook 'org-setup-company)
-  )
+;; (defun org-release ()
+;;   (let ((git-repo
+;;      (f-join user-emacs-directory "straight/repos/org")))
+;;     (s-trim (s-chop-prefix "release_"
+;;                (git-run "describe"
+;;                     "--match=release\*"
+;;                     "--abbrev=0"
+;;                     "HEAD")))))
 
 ;; overlay support
 ;; ----------------------------------------------------------------------------------------
@@ -217,6 +29,7 @@ This forces it to read the background before rendering."
 ;; https://kitchingroup.cheme.cmu.edu/blog/2016/11/06/Justifying-LaTeX-preview-fragments-in-org-mode/
 ;; https://github.com/jkitchin/scimax/blob/50e06f88299249a556825549818a8f79cba867e8/scimax-org.el#L585-L616
 
+;;;###autoload
 (defun org-latex-fragment-justify (justification)
   "Justify the latex fragment at point with JUSTIFICATION.
 JUSTIFICATION is a symbol for 'left, 'center or 'right."
@@ -248,15 +61,194 @@ JUSTIFICATION is a symbol for 'left, 'center or 'right."
       (when (>= offset 0)
     (overlay-put ov 'before-string (make-string offset ?\ ))))))
 
-(defun org-latex-fragment-justify-advice (beg end image &optional imagetype)
-  "After advice function to justify fragments."
-  (org-latex-fragment-justify 'center))
-
-(advice-add 'org--make-preview-overlay :after 'org-latex-fragment-justify-advice)
 
 
+(straight-use-package '(org :local-repo nil))
+(straight-use-package '(org-plus-contrib :local-repo nil))
 
 
+(use-package org
+  :defer t
+  :init
+  (setq org-src-window-setup 'split-window-below
+        org-hide-emphasis-markers nil
+        )
+  (setq-default org-latex-packages-alist
+                '(
+                  ("" "ctex" t nil)
+                  ("" "braket" t nil)
+                  ))
+
+  (setq-default org-preview-latex-image-directory
+                (concat user-emacs-directory ".cache/ltximg/"))
+  (setq-default org-latex-default-packages-alist
+                '(("AUTO" "inputenc" t
+                   ("xelatex"))
+                  ("T1" "fontenc" t
+                   ("xelatex"))
+                  ("" "graphicx" t nil)
+                  ("" "longtable" nil nil)
+                  ("" "wrapfig" nil nil)
+                  ("" "rotating" nil nil)
+                  ("normalem" "ulem" t nil)
+                  ("" "amsmath" t nil)
+                  ("" "textcomp" t nil)
+                  ("" "amssymb" t nil)
+                  ("" "capt-of" nil nil)
+                  ("" "hyperref" nil nil)))
+  (setq-default org-preview-latex-default-process 'dvipng)
+
+  :config
+  (defun org-latex-fragment-justify-advice (beg end image &optional imagetype)
+    "After advice function to justify fragments."
+    (org-latex-fragment-justify 'center))
+  (defun org-latex-preview-hook ()
+    "hook for auto `org-latex-preview'"
+    (when org-mode-hook
+      (when (and (not (equal (buffer-name) "*Youdao Dictionary*")))
+        (org-latex-preview '(16))))
+    )
+  (defun +org-refresh-latex-background-h (&rest args)
+    "Previews are rendered with the incorrect background.
+This forces it to read the background before rendering."
+    (plist-put+ org-format-latex-options
+                :background
+                (face-attribute (if-let (remap (cadr (assq 'default face-remapping-alist)))
+                                    (if (keywordp (car-safe remap))
+                                        (plist-get remap :background)
+                                      remap)
+                                  'default)
+                                :background nil t)))
+  (plist-put+ org-format-latex-options
+              :scale 2.0
+              ;; specify the justification you want
+              ;; :justify 'center
+              )
+  (advice-add 'org--make-preview-overlay :after 'org-latex-fragment-justify-advice)
+  (advice-add 'load-theme :after '+org-refresh-latex-background-h)
+  (add-hook 'org-mode-hook #'electric-pair-local-mode)
+  (add-hook 'org-mode-hook #'org-latex-preview-hook)
+  (add-hook 'org-mode-hook #'+org-refresh-latex-background-h)
+  (defun org-toggle-emphasis-markers ()
+    "Toggle markup display."
+    (interactive)
+    (if org-hide-emphasis-markers
+        (setq org-hide-emphasis-markers nil)
+      (setq org-hide-emphasis-markers t))
+    (font-lock-flush)
+    )
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (latex . t)   ;; <== add latex to the list
+     (python . t)
+     (shell . t)
+     ))
+  )
+
+
+;; (defun my-org-mode-hook ()
+;;   (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
+;; (add-hook 'org-mode-hook #'my-org-mode-hook)
+
+(use-package org-superstar
+  :after org
+  :init (add-hook 'org-mode-hook 'org-superstar-mode)
+  )
+
+(setq org-id-locations-file (concat my-cache-dir ".org-id-locations"))
+
+(use-package evil-org)
+
+(use-package org-roam
+  ;; :defer t
+  :after org
+  ;; :hook
+  ;; (after-init . org-roam-mode)
+  :commands (org-roam-buffer
+             org-roam-setup
+             org-roam-capture
+             org-roam-node-find)
+  :init
+  (setq org-roam-directory (expand-file-name "~/org/roam/")
+        org-roam-db-location (concat my-cache-dir "org-roam.db")
+        org-id-link-to-org-use-id
+        )
+  :config
+  (org-roam-setup)
+  (advice-add 'org-id-get-create :after 'org-roam-db-sync)
+  )
+
+
+;; better table alignment
+;; TODO integration with org-latex-preview
+(use-package valign
+  :straight (valign :host github
+                    :repo "casouri/valign"
+                    )
+  :defer t
+  :init
+  (setq
+   valign-fancy-bar t
+   )
+  (add-hook 'org-mode-hook #'valign-mode))
+
+;; (defun lsp-flycheck-filter-org-mode (oldfun &rest args)
+;;   (unless (equal (car args) 'org-mode)
+;;     (apply oldfun args)))
+
+
+;; (advice-add 'lsp-flycheck-add-mode :around
+;;             #'lsp-flycheck-filter-org-mode)
+
+
+;;; latex editing
+;;; ------------------------------------------------------------------------------------
+
+;; auto preview latex fragments
+
+
+
+;; set pdflatex to xelatex
+
+;; customize packages
+
+
+
+
+;; (straight-use-package '(my-org-edit-latex :local-repo "~/Projects/emacs/org-edit-latex/"))
+(use-package my-org-edit-latex
+  :straight (my-org-edit-latex :local-repo "~/Projects/emacs/org-edit-latex/")
+  :after org
+  :init
+  (setq org-edit-latex-default-frag-master
+    (concat user-emacs-directory "frag-master.tex"))
+  (add-hook 'org-mode-hook #'org-edit-latex-mode)
+  )
+
+
+(use-package org-ml
+  :after org
+  :straight (org-ml :type built-in
+                    :local-repo (concat user-emacs-directory "lisp/")
+                    :files ("org-ml.el"))
+  :init
+  (add-hook 'org-mode-hook 'org-ml-mode)
+  )
+
+(use-package company-org-latex
+  :after org
+  :straight (company-org-latex :type built-in
+                               :local-repo (concat user-emacs-directory "lisp/")
+                               :files ("company-org-latex.el"))
+  :init
+  (defun org-setup-company ()
+    (setq-local company-backends '(company-capf company-yasnippet company-dabbrev))
+    (add-hook 'completion-at-point-functions #'org-latex-capf nil t)
+    )
+  (add-hook 'org-mode-hook 'org-setup-company)
+  )
 
 ;;; keybinding
 ;;; ------------------------------------------------------------------------------------
@@ -299,5 +291,23 @@ JUSTIFICATION is a symbol for 'left, 'center or 'right."
   :keymaps 'org-src-mode-map
   ",," 'org-edit-src-exit
   ",k" 'org-edit-src-abort)
+
+
+(use-package markdown-mode
+  :defer t
+  :init
+  (setq markdown-hide-markup t
+        markdown-hide-markup-in-view-modes t
+        markdown-command "marked"
+        )
+  ;; (add-to-list 'auto-mode-alist '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . gfm-mode))
+  :config
+  (dwuggh/localleader-def
+   :keymaps 'markdown-mode-map
+   "tt" 'markdown-toggle-markup-hiding
+   "tp" 'markdown-toggle-inline-images
+   "tm" 'markdown-toggle-math
+   )
+  )
 
 (provide 'init-org)
